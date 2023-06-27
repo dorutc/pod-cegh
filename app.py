444019932e713ea37f8354b70ed1f00a2a099688
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 15:08:23 +0200

    x

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     p = subprocess.run("rm -rf /opt/app-root/src/doru/files_downloads/*")
-    p = subprocess.Popen("scrapy_list.sh", stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen("./scrapy_list.sh", stdout=subprocess.PIPE, shell=True)
     return p.communicate()            #"Hello World!"
 
 if __name__ == '__main__':
