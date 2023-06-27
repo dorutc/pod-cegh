e6c7b58ecaacb3e3ebc659dce7d8d2ac1e4f5ec9
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 18:17:37 +0200

    a

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
     try:
         os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
         str = "Hello World!" 
-        str = os.system("pwd")
+        #str = os.system("pwd")
         return str      #"Hello World!" 
     except Exception as error:
         return error
