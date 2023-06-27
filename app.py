1668ef2f6f0259386d881ee9ae8dbc36f5243340
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 16:33:35 +0200

    a

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
-	str = "Hello World!" 
+    str = "Hello World!" 
     with os.popen("cd cegh_doru;scrapy list") as f:
         str = f.readlines()
     return str      #"Hello World!" 
