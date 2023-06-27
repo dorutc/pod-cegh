7936d624e7f229ce1ca41a786e89b0885934eac2
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 18:55:14 +0200

    x

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
 with os.popen("cd cegh_doru;scrapy list") as f:
     str = f.readlines()
 print(type(str))
-for x in str
+for x in str:
    print(x)
