f8c7e7279a235c712f1524f5a878a060523b50d2
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 18:34:54 +0200

    x

--- test.py
+++ test.py
@@ -5,10 +5,7 @@ def hello():
     try:
         os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
         str = os.system("cd cegh_doru;scrapy list")
-        print(type(str))
-        lst = str.splitlines()
-        for x in lst:
-             print(x)		
+        print("type = " + type(str))	
         return "hello"      #"Hello World!" 
     except Exception as error:
         return error
