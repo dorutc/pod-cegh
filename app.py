8ff14bd3c6f7c985fe1cc4674b949908eb000182
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 18:03:24 +0200

    x

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
-    str = "Hello World!" 
-    with os.popen("cd cegh_doru;scrapy list") as f:
-        str = f.readlines()
-    return str      #"Hello World!" 
+    try:
+        os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
+        str = "Hello World!" 
+        with os.popen("cd cegh_doru;scrapy list") as f:
+            str = f.readlines()
+        return str      #"Hello World!" 
+    except Exception as error:
+        return error
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
