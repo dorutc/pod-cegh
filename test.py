2f3c51f70df33d357847527b808b6216bcfd9502
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 19:17:37 +0200

    x

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ with os.popen("cd cegh_doru;scrapy list") as f:
     str = f.readlines()
 print(type(str))
 for x in str:
-    x = x.x.strip("\n")
+    x = x.strip("\n")
     res = os.system("scrapy crawl " + x)
     print(x)
