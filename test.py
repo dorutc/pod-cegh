02eda20880716480f1f2728b253a524653a50b19
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-27 18:10:08 +0200

    a

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 
 import os
 
-
 def hello():
-    os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
-    str = "Hello World!" 
-    with os.popen("cd cegh_doru;scrapy list") as f:
-        str = f.readlines()
-    return str      #"Hello World!" 
-
-
-
+    try:
+        os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
+        str = "Hello World!" 
+        str = os.system("pwd")
+        return str      #"Hello World!" 
+    except Exception as error:
+        return error
 print(hello())
