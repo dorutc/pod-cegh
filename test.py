70a49428919533c609a4beb4dab4f37cc9a31510
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-28 13:56:39 +0200

    x

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 
 import os
 
+def do_multiple(src):
+    f = open(src, "r")
+    prinf(f.readline())
+
+
 os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
 with os.popen("cd cegh_doru;scrapy list") as f:
     str = f.readlines()
@@ -9,3 +14,7 @@ for x in str:
     x = x.strip("\n")
     res = os.system("cd cegh_doru;scrapy crawl " + x)
     print(x)
+
+
+f_list = os.listdir("/opt/app-root/src/doru/files_downloads")
+print(type(f_list)
