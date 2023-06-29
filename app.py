765bd9ce7264651e495b9d37100ac06a44c3913f
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-29 12:38:04 +0200

    x

--- app.py
+++ app.py
@@ -3,13 +3,54 @@ import os
 from datetime import date
 
 app = Flask(__name__)
+path = "/opt/app-root/src/doru/files_downloads/"
+header = ['Trading Day', 'Contract', 'Open', 'High', 'Low', 'Close', 'Trades', 'CEGHEDI', 'VWAP', 'CEGHIX', 'Settlement Price', 'Last Price', 'Price €/MWh', 'Product', 'Trading phase', 'Volume acc.']
+header_line = "Trading Day;Contract;Open;High;Low;Close;Trades;CEGHEDI;VWAP;CEGHIX;Settlement Price;Last Price;Price €/MWh;Product;Trading phase;Volume acc.;filename\n"
 today = date.today()
 str_date = today.strftime("%d%m%Y")
-path = "/opt/app-root/src/doru/files_downloads/"
+
+def do_multiple(src):
+    src1 = path + src
+    f = open(src1, "r")
+    dest1 = open(path + "all_" + str_date,"a")
+    h = f.readline()
+    h_list = h.split(";")
+    for l in f:
+        w_list = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+        l_list = l.split(";")
+        i = 0
+        for y in h_list:
+            j = 0
+            for x in header:
+                if x == y:
+                    w_list[j]= l_list[i]
+                j = j + 1
+            i = i + 1
+        w_l = ""
+        for z in w_list:
+            w_l = w_l + z + ";"
+        w_l += src
+        dest1.write(w_l + "\n")
+    dest1.close()
+    f.close()	
 
 @app.route('/')
 def hello():
     try:
+        os.system("rm -rf /opt/app-root/src/doru/files_downloads/*")
+        with os.popen("cd cegh_doru;scrapy list") as f:
+            str = f.readlines()
+        for x in str:
+            x = x.strip("\n")
+            res = os.system("cd cegh_doru;scrapy crawl " + x)
+
+        f_list = os.listdir(path)
+        dest = open(path + "all_" + str_date,"a")
+        dest.write(header_line)
+        dest.close()
+
+        for x in f_list:
+            do_multiple(x)
         page = ""
         f = open(path + "all_" + str_date,"r")
         for l in f:
